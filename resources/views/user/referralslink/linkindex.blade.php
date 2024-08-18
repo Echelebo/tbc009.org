7f86afd800d773efabe5e57ffa71c6f079475753
Echelebo <echelebochibuike@gmail.com>
2024-08-18 22:30:07 +0100

    First

--- resources/views/user/referralslink/linkindex.blade.php
+++ resources/views/user/referralslink/linkindex.blade.php
@@ -4,21 +4,7 @@
     <div class="container-fluid">
 
         <!-- start page title -->
-        <div class="row">
-            <div class="col-12">
-                <div class="page-title-box d-flex align-items-center justify-content-between">
-                    <h4 class="mb-0">Promotional Tools</h4>
 
-                    <div class="page-title-right">
-                        <ol class="breadcrumb m-0">
-                            <li class="breadcrumb-item"><a href="javascript: void(0);">Home</a></li>
-                            <li class="breadcrumb-item active">Promotional Tools</li>
-                        </ol>
-                    </div>
-
-                </div>
-            </div>
-        </div>
         <!-- end page title -->
         <div class="col-md-12">
             <div class="card">
@@ -26,6 +12,7 @@
                 <div class="card-body">
 
                     <div class="row">
+                        <p>Click on referral link to copy</p>
                         <div class="col-md-12"
                             data-copy="{{ route('user.register', ['ref' => user()->username ?? 'notset']) }}">
                             {{ route('user.register', ['ref' => user()->username ?? 'notset']) }}
