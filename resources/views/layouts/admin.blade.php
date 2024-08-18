f30864b73354eb0ef5ce019992e691a14efbc99b
Echelebo <echelebochibuike@gmail.com>
2024-08-18 14:32:29 +0100

    First

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -339,12 +339,12 @@
             <!--<a href="{{ route('admin.pages.index') }}"
                 class="block px-4 py-3 text-sm capitalize transition-colors duration-200 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white">
                 Pages
-            </a>
-
-            <a href="{{ route('admin.kyc.index') }}"
-                class="block px-4 py-3 text-sm capitalize transition-colors duration-200 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white">
-                KYC Records
             </a>-->
+
+            <a href="{{ route('admin.bots.index') }}"
+                class="block px-4 py-3 text-sm capitalize transition-colors duration-200 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white">
+                Bots
+            </a>
 
             <a href="{{ route('admin.settings.index') }}"
                 class="block px-4 py-3 text-sm capitalize transition-colors duration-200 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white">
@@ -413,13 +413,13 @@
             </a>
 
             {{-- p2p --}}
-            <!-- <a href="{{ route('admin.transfers.index') }}" class="ts-gray-1 rounded-full px-2 py-2">
+            <a href="{{ route('admin.bots.index') }}" class="ts-gray-1 rounded-full px-2 py-2">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-4 h-4">
                     <path fill-rule="evenodd"
                         d="M4.755 10.059a7.5 7.5 0 0112.548-3.364l1.903 1.903h-3.183a.75.75 0 100 1.5h4.992a.75.75 0 00.75-.75V4.356a.75.75 0 00-1.5 0v3.18l-1.9-1.9A9 9 0 003.306 9.67a.75.75 0 101.45.388zm15.408 3.352a.75.75 0 00-.919.53 7.5 7.5 0 01-12.548 3.364l-1.902-1.903h3.183a.75.75 0 000-1.5H2.984a.75.75 0 00-.75.75v4.992a.75.75 0 001.5 0v-3.18l1.9 1.9a9 9 0 0015.059-4.035.75.75 0 00-.53-.918z"
                         clip-rule="evenodd" />
                 </svg>
-            </a>-->
+            </a>
 
 
             {{-- Users --}}
